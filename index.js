fae1a72d318398fb77f98d96af4df25c8229551f
Ankush Chauhan <67872399+An-prog-hub@users.noreply.github.com>
2021-10-02 22:47:54 +0530

    bind the commit and added FILE_PATH to commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const jsonfile = require('jsonfile');
 const moment = require('moment');
 const simpleGit = require('simple-git')
 const random = require('random')
+const FILE_PATH = "./data.json";
+
 const makeCommit = n => {
     if(n===0) return simpleGit().push();
     const x = random.int(0,54);
@@ -12,6 +15,9 @@ const makeCommit = n => {
     }
     console.log(DATE);
 
+    jsonfile.writeFile(FILE_PATH, data, () => {
+        simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE});
+    });    
  
 } 
 
